514d2b7e749971a4057ddf1ba3452e82349b36e8
Dmitry <markelov16@gmail.com>
2022-10-08 00:49:30 +0500

    1 задача

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//задача 1
+Console.WriteLine("введите число:");
+int num = int.Parse(Console.ReadLine()!);
+if (num>99 && num<1000){
+    num=(num/10)%10;
+    Console.WriteLine($"второе число {num}");
+}
+else{
+    Console.WriteLine($"чсило {num} не является трехзначным");
+}
